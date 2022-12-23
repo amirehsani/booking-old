77ee63e8b11e1e4db01902b3ea978a3e126e8abd
Amir <amirmohammad7284@gmail.com>
2022-12-23 18:20:17 +0330

    Created reservation app

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from .models import *
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'phone_number',
+                    'email', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'first_name', 'last_name', 'nationality', 'date_of_birth', 'is_active')
+    list_display = ('id', 'user', 'name', 'nationality', 'date_of_birth', 'is_active')
     list_filter = ('is_active',)
-    search_fields = ('user', 'first_name', 'last_name', 'nationality')
+    search_fields = ('user', 'name', 'nationality')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+
 from django.db import models
 
 
@@ -10,25 +11,25 @@ class User(AbstractUser):
     is_anonymous = models.BooleanField(default=True)
     is_authenticated = models.BooleanField(default=False)
 
-    # USERNAME_FIELD = username
-    # REQUIRED_FIELDS = name
+    # REQUIRED_FIELDS and USERNAME_FIELD are already in AbstractUser
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField()
-    email = models.EmailField(max_length=255)
-    first_name = models.CharField(max_length=60, verbose_name='First Name')
-    last_name = models.CharField(max_length=100, verbose_name='Last Name')
+    avatar = models.ImageField(upload_to='users/user_avatars/')
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=200)
     nationality = models.CharField(max_length=64, null=True, blank=True)
-    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name='Date of Birth')
 
     created_time = models.DateTimeField(auto_now=True)
     modified_time = models.DateTimeField(auto_now_add=True)
 
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.user.username
 
-# class UserManager(models.Model):
+
+# TODO class UserManager(models.Model):

150503ebc6d2c506c29e0672136de063e8dbd37d
Amir <amirmohammad7284@gmail.com>
2023-01-01 18:54:50 +0330

    Installed Swagger and added configurations

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,10 @@ urlpatterns = [
     path('', include('residence.urls')),
     path('', include('reservation.urls')),
     path('', include('cart.urls')),
+
+    # URL
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 admin.site.site_header = 'Booking Administration Panel'

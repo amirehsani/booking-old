37493338be58dd43d5c27757afb55b9ab3ab9310
Amir <amirmohammad7284@gmail.com>
2023-01-17 22:00:20 +0330

    Modified database, added a temp one

--- core/settings.py
+++ core/settings.py
@@ -74,6 +74,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',

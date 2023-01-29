3b8f585917c3344892731d776e99c4a3775f1b94
Amir <amirmohammad7284@gmail.com>
2023-01-30 00:02:37 +0330

    Added a new sample functional view using Redis caching

--- air/views.py
+++ air/views.py
@@ -1,15 +1,39 @@
+import json
+
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import *
+from abstract.redis_client import redis_client
 
 
-class AirLineDisplay(ListAPIView):
-    queryset = Airline.objects.all()
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [AllowAny]
-    serializer_class = AirlineSerializer
+# WE'RE TESTING REDIS FOR OUR DJANGO VIEWS USING DRF FUNCTIONAL VIEWS.
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def airline_display(request):
+    if request.method == 'GET':
+        airline = redis_client.get('arline')
+
+        if airline is None:
+            print("Couldn't find data in cache, retrieving from database...")
+            airline = Airline.objects.all()
+            redis_client.setex('airline', 60*60*24*7, json.dumps(airline))
+
+        redis_client.get('airline')
+        serializer = AirlineSerializer(airline)
+        return Response(serializer.data)
+
+# TODO Change the default view back to DRF CBV with caching capabilities.
+
+# class AirLineDisplay(ListAPIView):
+#     queryset = Airline.objects.all()
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes = [AllowAny]
+#     serializer_class = AirlineSerializer
 
 
 class AirplaneDisplay(ListAPIView):
